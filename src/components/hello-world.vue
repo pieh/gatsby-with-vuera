036bbcca955b7f9bcd1fe734b9db2c86e983fc90
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-07-27 04:11:44 +0200

    why not vue?

---
+++ src/components/hello-world.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ greeting }} World! It's hot reloading</p>
+</template>
+
+<script>
+module.exports = {
+  props: ["greeting"],
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { VueWrapper } from "vuera"
+import MyVueComponent from "../components/hello-world.vue"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [greeting, setGreeting] = useState("Hello")
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <fieldset>
+        <legend>React</legend>
+        <input
+          type="text"
+          value={greeting}
+          onChange={e => setGreeting(e.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Vue</legend>
+        <VueWrapper component={MyVueComponent} greeting={greeting} />
+      </fieldset>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
